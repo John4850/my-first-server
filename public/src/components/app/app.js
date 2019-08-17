cd146eb033becfe7c8cb7f889748f4ac710c5b0f
JOHN NELSON <JohnNelson4850@gmail.com>
2019-08-17 15:03:08 -0700

    pokedex update

--- public/src/components/app/app.js
+++ public/src/components/app/app.js
@@ -1,5 +1,8 @@
-import Component from '..Component.js';
+import Component from '../../components/Component.js';
 import Header from '../header/header.js';
+import getPokemon from '../../services/services.js';
+import PokemonBox from '../poke-box/poke-box.js';
+// import hashStorage from '../../services/hash-storage.js';
 
 
 class App extends Component {
@@ -8,6 +11,21 @@ class App extends Component {
         const header = new Header();
         const headerDOM = header.renderDOM();
         dom.prepend(headerDOM);
+
+        const pokemonBox = new PokemonBox({ pokemonList: [] });
+        const main = dom.querySelector('#main');
+        main.appendChild(pokemonBox.renderDOM());
+
+        function loadPokemon() {
+            // const options = hashStorage.get();
+            getPokemon()
+                .then(data => {
+                    const pokemonList = data.results.results;
+                    pokemonBox.update({ pokemonList: pokemonList });
+                });
+
+        }
+        loadPokemon();
     }
 
 
